b97fd681c25069b260d34c016c26bb80007436e2
Devender Reddy <kdreddy582@gmail.com>
2023-03-25 22:45:57 +0530

    hi
    hi

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'fitpro';
+  message = "Hello Angular";
+  saySomeThing = " Hello World";
+  city:string ="Hyderabad";
+  imgPath: string = "../assets/pongal-whishes.jpg";
+  saySomethingCls: string = "special pecial";
+  //customDate: Date = new Date();
+  //customDate: string = new Date().toLocaleDateString();
+  customDate: string = new Date().toLocaleTimeString();
+
+  timeid = setInterval(() => {
+    this.customDate = new Date().toLocaleTimeString();
+  }, 1000);
+
+  test(){
+    return 200
+  }
+}
